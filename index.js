c12d07e327b0f085876217574e8114548870bd4c
john kim <jhoney510@gmail.com>
2020-07-06 23:33:44 +0900

    Add config

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 
-const api_util           = require('./lib/utils/api_util');
-const db_util            = require('./lib/utils/db_util');
-const jwt_util           = require('./lib/utils/jwt_util');
-const mongo_archive_util = require('./lib/utils/mongo_archive_util');
-const none_archive_util  = require('./lib/utils/none_archive_util');
-const route_util         = require('./lib/utils/route_util');
-const express_router     = require('./lib/express_route_maker');
-
 
 module.exports = {
-  api_util,
-  db_util,
-  jwt_util,
-  mongo_archive_util,
-  none_archive_util,
-  route_util,
+  get config () { return require('./lib/config'); },
 
-  express_router,
+  get api_util           () { return require('./lib/utils/api_util'          ); },
+  get db_util            () { return require('./lib/utils/db_util'           ); },
+  get jwt_util           () { return require('./lib/utils/jwt_util'          ); },
+  get mongo_archive_util () { return require('./lib/utils/mongo_archive_util'); },
+  get none_archive_util  () { return require('./lib/utils/none_archive_util' ); },
+  get route_util         () { return require('./lib/utils/route_util'        ); },
+
+  get express_router () { return require('./lib/express_route_maker'); },
 };
